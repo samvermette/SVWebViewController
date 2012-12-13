aaf251e9376021383a7896a48a44ef293bc8f505
Ben Pettit <benpettit@digimulti.com>
2012-12-13 18:30:28 +1100

    Fixed the SVModalWebViewController to resize on landscape orientation.

--- SVWebViewController/SVWebViewController.m
+++ SVWebViewController/SVWebViewController.m
@@ -143,6 +143,7 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
     [self updateToolbarItems];
+    [self.navigationController.view.superview setAutoresizingMask:UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight];
 }
 
 - (void)viewDidUnload {
