49d021b1fed7b35413c395209028a8fdb8b6fea2
Ben Pettit <benpettit@digimulti.com>
2013-01-15 15:32:35 +1100

    Created the SVWebViewControllerDelegate protocol to use a function to create the custom nav bar button.

---
+++ SVWebViewController/SVWebViewControllerDelegate.h
@@ -0,0 +1,12 @@
+//
+//  SVWebViewControllerDelegate.h
+//  Videohog-iOS
+//
+//  Created by Ben Pettit on 14/01/13.
+//  Copyright 2012 Digimulti. All rights reserved.
+//
+
+@protocol SVWebViewControllerDelegate <NSObject>
+@optional
+- (UIBarButtonItem *)createCustomBarButton;
+@end
