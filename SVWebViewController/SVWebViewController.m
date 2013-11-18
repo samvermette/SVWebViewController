4d0a3f7ff4d053db51f278019772333c66e9a2aa
Matthew Strickland <matthew@idlefusion.com>
2013-11-18 10:28:41 -0500

    Set the toolbar's barTintColor based on the navigation bar.

--- SVWebViewController/SVWebViewController.m
+++ SVWebViewController/SVWebViewController.m
@@ -200,6 +200,7 @@
         toolbar.items = items;
         toolbar.barStyle = self.navigationController.navigationBar.barStyle;
         toolbar.tintColor = self.navigationController.navigationBar.tintColor;
+	toolbar.barTintColor = self.navigationController.navigationBar.barTintColor;
         self.navigationItem.rightBarButtonItems = items.reverseObjectEnumerator.allObjects;
     }
     
@@ -218,6 +219,7 @@
         
         self.navigationController.toolbar.barStyle = self.navigationController.navigationBar.barStyle;
         self.navigationController.toolbar.tintColor = self.navigationController.navigationBar.tintColor;
+	self.navigationController.toolbar.barTintColor = self.navigationController.navigationBar.barTintColor;
         self.toolbarItems = items;
     }
 }
